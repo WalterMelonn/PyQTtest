b45893ca6e785a4e9f7c2512df0f9f5a36733c60
WalterMelonn <76143859+WalterMelonn@users.noreply.github.com>
2021-02-03 23:41:36 +0300

    Почти обуздал заполнение комбобоксов, осталось прикрутить: - цикл запаковки Выбираемых элементов в список; - цикл распаковки ^этого - чтобы по нажатию кнопки все делалось

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
 from PyQt5.QtCore import QCoreApplication
 
-
-class Example(QWidget):
+combobox_need = 14
 
 
 class DynamicComboBoxes(QtWidgets.QWidget):
@@ -16,9 +15,16 @@ class DynamicComboBoxes(QtWidgets.QWidget):
         super(DynamicComboBoxes, self).__init__(parent)
         vbox = QtWidgets.QVBoxLayout(self)
         spinbox = QtWidgets.QSpinBox(self)
-        spinbox.setRange(0, 10)
+        spinbox.setRange(0, combobox_need)
         spinbox.valueChanged.connect(self.onChangeValue)
+
+        qbtn = QPushButton('Рассчитать', self)
+        qbtn.clicked.connect(self.onBtnClick)
+        self.itemlist = []
+
+        vbox.addWidget(qbtn)
         vbox.addWidget(spinbox)
+
         self.grid = QtWidgets.QGridLayout()
         self.itemlist = []
         vbox.addLayout(self.grid)
@@ -35,11 +41,38 @@ class DynamicComboBoxes(QtWidgets.QWidget):
             self.grid.addWidget(label, i, 0)
             self.grid.addWidget(combobox, i, 1)
             self.itemlist.append([label, combobox])
+            # print(self.itemlist[i][1])
+        """
+        for 
+            try:
+                self.itemlist[i-1][1].addItems(["DEFAULT", "PATCH"])
+                self.itemlist[i][1].addItems(["1111", "2222"])
+            except:
+                print(Exception)
+            print("Завершение программы")
+        """
+        for i in range(2):
+            try:
+               # DynamicComboBoxes.itemlist[i - 1][1].addItems(["1", "2", "3", "4", "5"])
+                self.itemlist[0][1].addItems(["1"])
+                self.itemlist[1][1].addItems(["2"])
+            except:
+                print(Exception)
+            print("Завершение программы")
+
+    def onBtnClick(self):
+        for i in range(2):
+            try:
+               # DynamicComboBoxes.itemlist[i - 1][1].addItems(["1", "2", "3", "4", "5"])
+                self.itemlist[0][1].addItems(["1"])
+                self.itemlist[1][1].addItems(["2"])
+            except:
+                print(Exception)
+            print("Завершение программы")
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
     box = DynamicComboBoxes()
     box.show()
     sys.exit(app.exec_())
